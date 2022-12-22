051ccb57743831363105a6e525bfec342e8ebb96
Rajat <rajatlinux@hotmail.com>
2022-12-22 13:14:15 +0530

    fix-code

--- main.py
+++ main.py
@@ -9,102 +9,153 @@ import glob
 import environ
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent
-
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env('config.env')
-
-EXCEL_FILE = glob.glob(f"{str(BASE_DIR)}/*.xlsx")[0]
-xls_data = pandas.read_excel(EXCEL_FILE)
-receivers_data = xls_data.to_dict('records')
-
-
 sender = env("EMAIL_HOST_USER")
 password = env("EMAIL_HOST_PASSWORD")
 
 
-# Setup the MIME
-message = MIMEMultipart()
-message['From'] = sender
-files_size = 0
+def home():
+    env_file = glob.glob(f"{str(BASE_DIR)}/*.env")
+    excel_file = glob.glob(f"{str(BASE_DIR)}/*.xlsx")
+    all_files = glob.glob(f"{str(BASE_DIR)}/*")
+    directory = []
+    EXCEL_FILE_NAME = ''
+    ATTACHMENT_DIR_NAME = ''
+    message = MIMEMultipart()
+    message['From'] = sender
 
-all_files = glob.glob(f"{str(BASE_DIR)}/attachments/*")
+    if not env_file:
+        print("""Please configure .env file in base directory with these variables
+        EMAIL_HOST_USER=
+        EMAIL_HOST_PASSWORD=
+        SMTP_NAME=
+        SMTP_PORT=
+        """)
+        return None
 
-has_zip = False
-files_names = []
-print("\n")
-for file in all_files:
-    if os.path.isfile(file):
-        file_name = file.split('/')[-1]
-        if file_name.endswith(".zip") or file_name.endswith(".tar") or file_name.endswith(".rar"):
-            print(f">>\"{file_name}\" not attached")
-            has_zip = True
+    if not excel_file:
+        print("Please provide emails excel file in base directory")
+        return None
 
-        file_stats = os.stat(file)
-        files_size += file_stats.st_size
+    for file in all_files:
+        if not os.path.isfile(file):
+            directory.append(file)
 
-if has_zip:
-    # print("You can't attach any compressed file for security reasons, You have to uncompressed them")
-    print(f"\033[91m You can't attach any compressed file for security reasons, You have to uncompressed them \033[0m", )
+    for position, name in enumerate(excel_file, start=1):
+        print(position, name.split("/")[-1])
 
-if files_size > 25000000:
-    print(
-        f"\nAttached files size too large, you attached {(files_size/1000)/1000:.1f} MB and it accepts only 25 MB")
+    desired_number = input("Chose your desire excel file : ")
 
-else:
-    print(f"Attached files size {(files_size/1000)/1000:.1f} MB")
-    for data in receivers_data:
-        receiver = data['Email']
-        body = f'''Hello {data["First Name"]},
-        This is the body of the email
-        Sincerely,
-        Testing
-        '''
-        message['To'] = receiver
-        message['Subject'] = 'This is a testing mail from RJT'
+    if desired_number.isdigit():
+        EXCEL_FILE_NAME = excel_file[int(desired_number)-1]
 
-        message.attach(MIMEText(body, 'plain'))
+    for position, name in enumerate(directory, start=1):
+        print(position, name.split("/")[-1])
 
-        for file in all_files:
-            if os.path.isfile(file):
-                file_name = file.split('/')[-1]
-                file_path = file
-                # open the file in bynary
-                if file_name.endswith(".zip") or file_name.endswith(".tar") or file_name.endswith(".rar"):
-                    continue
-                else:
-                    files_names.append(file_name)
-                    binary_pdf = open(file_path, 'rb')
-                    payload = MIMEBase(
-                        'application', 'octate-stream', Name=file_name)
-                    # payload = MIMEBase('application', 'pdf', Name=file_name)
-                    payload.set_payload((binary_pdf).read())
+    desired_number = input("Chose your desire attachment directory : ")
 
-                    # enconding the binary into base64
-                    encoders.encode_base64(payload)
+    if desired_number.isdigit():
+        ATTACHMENT_DIR_NAME = directory[int(desired_number)-1]
 
-                    # add header with pdf name
-                    payload.add_header('Content-Decomposition',
-                                    'attachment', filename=file_name)
-                    message.attach(payload)
+    action(EXCEL_FILE_NAME, ATTACHMENT_DIR_NAME)
 
-        # use gmail with port
-        session = smtplib.SMTP(env("SMTP_NAME"), env("SMTP_PORT"))
 
-        # enable security
-        session.starttls()
+def action(excel, attachment):
+    EXCEL_FILE = glob.glob(f"{excel}")[0]
+    xls_data = pandas.read_excel(EXCEL_FILE)
+    receivers_data = xls_data.to_dict('records')
 
-        # login with mail_id and password
-        session.login(sender, password)
+    sender = env("EMAIL_HOST_USER")
+    password = env("EMAIL_HOST_PASSWORD")
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender
+    files_size = 0
 
-        text = message.as_string()
-        session.sendmail(sender, receiver, text)
-        print(f"Mail sent to {data['First Name']}")
+    all_files = glob.glob(f"{attachment}/*")
+    print("all_files === ", all_files)
+    # return None
+    # print("all_filespath", all_files)
 
-    session.quit()
-    for position, file in enumerate(files_names, start=1):
-        print("\n")
-        print(f"\033[1m {position} - {file}\033[0m", )
-    
-    print("\033[92m All mail sent successfully \033[0m", )
+    has_zip = False
+    files_names = []
+
+    for file in all_files:
+        if os.path.isfile(file):
+            file_name = file.split('/')[-1]
+            if file_name.endswith(".zip") or file_name.endswith(".tar") or file_name.endswith(".rar"):
+                print(f">>\"{file_name}\" not attached")
+                has_zip = True
+
+            file_stats = os.stat(file)
+            files_size += file_stats.st_size
+
+    if has_zip:
+        print(f"\033[91m You can't attach any compressed file for security reasons, You have to uncompressed them \033[0m", )
+
+    if files_size > 25000000:
+        print(
+            f"\nAttached files size too large, you attached {(files_size/1000)/1000:.1f} MB and it accepts only 25 MB")
+
+    else:
+        print(f"Attached files size {(files_size/1000)/1000:.1f} MB")
+        for data in receivers_data:
+            receiver = data['Email']
+            body = f'''Hello {data["First Name"]},
+            This is the body of the email
+            Sincerely,
+            Testing
+            '''
+            message['To'] = receiver
+            message['Subject'] = 'This is a testing mail from RJT'
+
+            message.attach(MIMEText(body, 'plain'))
+
+            for file in all_files:
+                if os.path.isfile(file):
+                    file_name = file.split('/')[-1]
+                    file_path = file
+                    # open the file in bynary
+                    if file_name.endswith(".zip") or file_name.endswith(".tar") or file_name.endswith(".rar"):
+                        continue
+                    else:
+                        files_names.append(file_name)
+                        binary_pdf = open(file_path, 'rb')
+                        payload = MIMEBase(
+                            'application', 'octate-stream', Name=file_name)
+                        # payload = MIMEBase('application', 'pdf', Name=file_name)
+                        payload.set_payload((binary_pdf).read())
+
+                        # enconding the binary into base64
+                        encoders.encode_base64(payload)
+
+                        # add header with pdf name
+                        payload.add_header('Content-Decomposition',
+                                           'attachment', filename=file_name)
+                        message.attach(payload)
+
+            # use gmail with port
+            session = smtplib.SMTP(env("SMTP_NAME"), env("SMTP_PORT"))
+
+            # enable security
+            session.starttls()
+
+            # login with mail_id and password
+            session.login(sender, password)
+
+            text = message.as_string()
+            session.sendmail(sender, receiver, text)
+            print(f"Mail sent to {data['First Name']}")
+
+        session.quit()
+        for position, file in enumerate(files_names, start=1):
+            print("\n")
+            print(f"\033[1m {position} - {file}\033[0m", )
+
+        print("\033[92m All mail sent successfully \033[0m", )
+
+
+if __name__ == "__main__":
+    home()
